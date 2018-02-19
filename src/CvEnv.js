7d74bea82d97ea84cca52e664530dd6e966168ce
Benomas <benomas@gmail.com>
2018-02-19 13:11:31 -0600

    Environment fix

--- src/CvEnv.js
+++ src/CvEnv.js
@@ -1,24 +1,13 @@
 export default function(){
 	this.environmentProperty=(property,defaultValue)=>{
-		if(typeof process==="undefined" || typeof process.env==="undefined" || typeof process.env.NODE_ENV==="undefined")
-			return defaultValue;
-		
-		if(process.env.NODE_ENV==="development" && typeof process.env[property]!=="undefined")
-			return process.env[property];
-		else
-			return defaultValue;
-
-		if(process.env.NODE_ENV==="production" && typeof process.env[property]!=="undefined")
-			return process.env[property];
-		else
-			return defaultValue;
+		return process.env[property] || defaultValue
 	};
 
 	this.url=()=>{
 		return this.environmentProperty("URL","/");
 	};
 	this.apiUrl=()=>{
-		return this.environmentProperty("API_URL","/");
+		return this.environmentProperty("API_URL","/asdasdas");
 	};
 
 	this.isPassportEnabled=()=>{
