250dd44aa0c0e75763a329cac6db8303dd33f949
Benomas <benomas@gmail.com>
2018-05-26 06:42:10 -0500

    -Recursive vue component class Initialized

---
+++ src/VueChromosome.js
@@ -0,0 +1,13 @@
+//Global Dinamic dependencies
+//To complete
+export default class CvClass {
+  constructor(...options){
+    this.data     = function () { return { row:{} } }
+    this.props    = []
+    this.computed = {}
+    this.components         = {}
+    if (options != null){
+
+    }
+  }
+};
