82dd75c704ae63e26929d8b5933d63fd76a687f2
Benomas <benomas@gmail.com>
2018-09-27 15:48:56 -0500

    -Font sizes changed -Authenticated and Unauthenticated redirects

--- src/network/CvComunicator.js
+++ src/network/CvComunicator.js
@@ -5,11 +5,11 @@ import CvCrudService from './CvCrudService'
 import cvSerialize   from '../cvSerialize'
 
 export default function(router,globals){
-  this.globals = globals;
-  this.cvDinDep = cvDinDep;
-  this.resources={};
-  this.cvEnv = this.cvDinDep("CvEnv",globals) || new CvEnv();
-  var axios = require('axios');
+  this.globals       = globals
+  this.cvDinDep      = cvDinDep
+  this.resources     = {}
+  this.cvEnv         = this.cvDinDep("CvEnv",globals) || new CvEnv()
+  var axios          = require('axios')
   this.defaultConfig = {
     baseURL            : this.cvEnv.apiUrl(),
     headers            : {
@@ -20,10 +20,10 @@ export default function(router,globals){
     responseType      : 'json',
     maxContentLength  : this.cvEnv.ajaxMaxContentLenght() || 20000,
     callBackRedirector: null
-  };
-  this.router     = router;
-  this.axios      = axios.create(this.defaultConfig);
-  this.cvPassport = this.cvDinDep("CvPassport",globals) || new CvPassport();
+  }
+  this.router     = router
+  this.axios      = axios.create(this.defaultConfig)
+  this.cvPassport = this.cvDinDep("CvPassport",globals) || new CvPassport()
 
   this.shareHeaders = function () {
     let headers = {
@@ -31,49 +31,49 @@ export default function(router,globals){
       'Content-Type'               : 'application/json'
     }
     //let headers = JSON.parse(JSON.stringify(this.defaultConfig.headers))
-    return this.cvPassport.injectHeaders(headers);
+    return this.cvPassport.injectHeaders(headers)
   }
 
   this.axios.interceptors.request.use((config)=>{
     if(this.cvPassport.autenticated())
-     config.headers = this.cvPassport.injectHeaders(config.headers);
-    return config;
+     config.headers = this.cvPassport.injectHeaders(config.headers)
+    return config
   },(error)=> {
-    return Promise.reject(error);
-  });
+    return Promise.reject(error)
+  })
 
   this.axios.interceptors.response.use((response)=>{
-      this.cvPassport.reactToResponse(response);
-      return response;
+      this.cvPassport.reactToResponse(response)
+      return response
   },(error)=>{
-    this.cvPassport.reactToResponse(error);
+    this.cvPassport.reactToResponse(error)
     this.proccessErrorStatus(error.response)
-    return Promise.reject(error);
-  });
+    return Promise.reject(error)
+  })
 
   this.pushStaticCrudServices=(resource)=>{
     if(typeof resource==="undefined")
-      return false;
-    this.resources[resource.resourceName] =  this.cvDinDep("CvCrudService",globals) || new CvCrudService(this,resource);
-  };
+      return false
+    this.resources[resource.resourceName] =  this.cvDinDep("CvCrudService",globals) || new CvCrudService(this,resource)
+  }
 
   this.pushDinamicCrudServices=(resource)=>{
     if(typeof resource==="undefined")
-      return false;
-    this.resources[resource.resourceName] = resource;
-  };
+      return false
+    this.resources[resource.resourceName] = resource
+  }
 
   this.proccessErrorStatus = (response) => {
     switch(response.status){
       case 401:
-        this['error' + response.status]();
-        return true;
+        this['error' + response.status]()
+        return true
       case 403:
-        this['error' + response.status]();
-        return true;
+        this['error' + response.status]()
+        return true
       case 409:
-        this['error' + response.status]();
-        return true;
+        this['error' + response.status]()
+        return true
     }
     return false
   }
@@ -84,7 +84,7 @@ export default function(router,globals){
   }
 
   this.error401 = () => {
-    this.router.VueRouter.push('/login');
+    this.router.VueRouter.push('/login')
   }
 
   this.error403 = () => {
@@ -93,7 +93,18 @@ export default function(router,globals){
   }
 
   this.error409 = () => {
-    location.reload();
+    location.reload()
   }
   this.cvSerialize=cvSerialize
-};
+
+  this.loguedStart    = () => {
+    let redirect = this.router != null &&
+    this.router.loguedStart != null ? this.router.loguedStart():'/'
+    this.redirect(redirect)
+  }
+  this.unLoguedStart  = () =>{
+    let redirect = this.router != null &&
+    this.router.unLoguedStart != null ? this.router.unLoguedStart():'/'
+    this.redirect(redirect)
+  }
+}
