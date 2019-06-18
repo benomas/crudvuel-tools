55b33e1e7f786195773ac250f93ce3353ff07498
Beny michel velasco <benomas@gmail.com>
2019-06-17 22:48:48 -0500

    remove last_name from roles and permissions

--- src/themes/quasar/components/security/roles/CvIndex.vue
+++ src/themes/quasar/components/security/roles/CvIndex.vue
@@ -23,8 +23,6 @@
           {{ fLang('active') }}
           <cv-order-icons :cv-data="props.gridData" cv-key="active"></cv-order-icons>
         </th>
-        <th cv-key="last_name" class="cv-filterable cv-selectable " hidden>
-        </th>
     </template>
     <!-- cv-grid-extra-headers-->
     <template v-if="1" slot="extra-actions-header-slot" slot-scope="props">
