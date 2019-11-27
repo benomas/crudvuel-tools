015b147c61404bc6c66bdd830619b284b2de9608
Benomas <benomas@gmail.com>
2019-11-27 05:31:28 -0600

    add last changes with fast commit

--- src/themes/quasar/components/security/users/CvCreate.vue
+++ src/themes/quasar/components/security/users/CvCreate.vue
@@ -49,7 +49,7 @@
           </q-input>
         </q-field>
       </div>
-      <div class="ccol-xs-12 col-sm-6 col-md-4 q-pa-md m-auto">
+      <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md">
         <q-field v-bind="defErrorInputProps('active')">
           <cv-toggle v-bind="defInputProps('active')" v-model="row.active" :left-label="true" :true-value="1" :false-value="0" color="positive"/>
         </q-field>
