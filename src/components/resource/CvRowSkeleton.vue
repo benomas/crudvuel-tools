5cbe0be73bb0682bbeb521b088841c9db3fa8b62
Benomas <benomas@gmail.com>
2020-01-10 18:14:59 -0600

    add last changes with fast commit

---
+++ src/components/resource/CvRowSkeleton.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data () {
+    return {
+      data      : {},
+      resource  : null,
+      isMounted : false
+    }
+  },
+  props: [
+    'cvRow',
+    'cvContainerResource',
+    'cvCurrentResource'
+  ],
+  computed: {
+    cContainerResource: function () {
+      return this.cvContainerResource
+    },
+    cCurrentResource: function () {
+      return this.cvCurrentResource
+    }
+  },
+  methods: {
+    validator: function () {
+      return true
+    }
+  },
+  created: function () {
+    this.resource = this.resources.locations
+    this.action = this.resource.actions.create
+  },
+  mounted: function () {
+    this.isMounted = true
+  }
+}
+</script>
