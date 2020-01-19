99be7c82a56e1cd23ca6a3c6a0d995beaa7671c3
Benomas <benomas@gmail.com>
2020-01-18 21:08:58 -0600

    add last changes with fast commit

--- src/components/grid-components/CvSimpleFiltersLoader.js
+++ src/components/grid-components/CvSimpleFiltersLoader.js
@@ -1,5 +1,19 @@
+export default (()=>{
+  let toExport
+  try{
+    toExport = require('src/customs/crudvuel/themes/quasar/components/grid-components/CvSimpleFilters')
+  }catch(e){
 
-src/customs/crudvuel/themes/quasar/components/grid-components/CvSimpleFilters.vue
-export default {
+  }
+  try{
+    toExport = require('crudvuel-tools/src/themes/quasar/components/grid-components/CvSimpleFilters')
+  }catch(e){
 
-}
+  }
+  try{
+    toExport = require('crudvuel-tools/src/components/grid-components/CvSimpleFilters')
+  }catch(e){
+    console.log('No definition to be loaded')
+  }
+  return toExport.default
+})()
