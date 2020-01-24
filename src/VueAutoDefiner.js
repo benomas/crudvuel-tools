cd6edda9cb5f6fdba2712d231aed066351837efd
Benomas <benomas@gmail.com>
2020-01-24 06:57:22 -0600

    add last changes with fast commit

---
+++ src/VueAutoDefiner.js
@@ -0,0 +1,322 @@
+import {camelCase,split,kebabCase,lowerCase} from 'lodash'
+export default class VueAutoDefiner {
+  constructor () {
+    this.autoProps        = []
+    this.autoComputed     = {}
+    this.autoData         = {}
+    this.autoMethods      = {}
+    this.autoMounters     = []
+    this.autoMounted      = {}
+    this.resetAutoDefs()
+    this.validScopes      = {gen:true,com:true,comf:true,ins:true,insf:true}//comf component fixed
+    this.validModes       = {sta:true,din:true}
+  }
+
+  inyectComponentConnector (componentConnector = null) {
+    this.componentConnector = componentConnector
+    return this
+  }
+
+  noTypes () {
+    return {'D':false,'P':false,'EM':false,'M':false}
+  }
+
+  allTypes () {
+    return {'D':true,'P':true,'EM':false,'M':true}
+  }
+
+  resetAutoDefs(){
+    this.autoDefTypes    = this.noTypes()
+    this.autoDefMode     = null//din(dinamic),sta(static)
+    this.autoDefScope    = null//gen(general scope),com(component scope),ins(instance scope)
+    this.autoDefName     = null
+    this.autoDefInit     = null
+    this.autoDefDataInit = null
+    this.autoDefPropInit = null
+    return this
+  }
+
+  getAutoDefConf () {
+    return {
+      types : this.autoDefTypes,
+      mode  : this.autoDefMode,
+      scope : this.autoDefScope,
+      name  : this.autoDefName,
+      def   : this.autoDefInit
+    }
+  }
+
+  getAutos () {
+    let cData = this.autoData
+    return {
+      data () {
+        return cData
+      },
+      props    : this.autoProps,
+      computed : this.autoComputed,
+      methods  : this.autoMethods,
+      mounters : this.autoMounters,
+      mounted  : this.autoMounted
+    }
+  }
+  getAutoDefDataInit (){
+    return this.autoDefDataInit
+  }
+  getAutoDefPropInit (){
+    return this.autoDefPropInit
+  }
+  getAutoDefTypes () {
+    return this.autoDefTypes
+  }
+  getAutoDefMode () {
+    return this.autoDefMode
+  }
+  getAutoDefScope () {
+    return this.autoDefScope
+  }
+  getAutoDefName () {
+    return this.autoDefName
+  }
+  getAutoDefInit () {
+    return this.autoDefInit
+  }
+
+  hasAutoData(){
+    return this.getAutoDefTypes()['D']
+  }
+  hasAutoProp(){
+    return this.getAutoDefTypes()['P']
+  }
+  hasAutoEmitter(){
+    return this.getAutoDefTypes()['EM']
+  }
+  setAutoDefDataInit (autoDefDataInit = null) {
+    this.autoDefDataInit = autoDefDataInit
+    return this
+  }
+  setAutoDefPropInit (autoDefPropInit = null) {
+    this.autoDefPropInit = autoDefPropInit
+    return this
+  }
+  setAutoDefTypes (autoDefTypes = null) {
+    this.autoDefTypes = autoDefTypes
+    return this
+  }
+  setAutoDefMode (autoDefMode = '') {
+    this.autoDefMode = lowerCase(autoDefMode)
+    return this
+  }
+  setAutoDefScope (autoDefScope = '') {
+    this.autoDefScope = lowerCase(autoDefScope)
+    return this
+  }
+  setAutoDefName (autoDefName = null) {
+    this.autoDefName = camelCase(autoDefName)
+    return this
+  }
+  setAutoDefInit (autoDefInit = null) {
+    this.autoDefInit = autoDefInit
+    return this
+  }
+  loadAutoDefTypes (source) {
+    let cTypes = (source[2] == null || source[2] === '') ? ['D']:split(source[2],'|')
+    let types = this.noTypes()
+    for (const type of cTypes)
+      if(types[type] != null)
+        types[type] = true
+    return this.resetAutoDefs().setAutoDefTypes(types)
+  }
+  loadAutoDefMode (source){
+    if(source[3] == null)
+      return this
+    return this.setAutoDefMode(source[3])
+  }
+  loadAutoDefScope (source){
+    if(source[4] == null)
+      return this
+    return this.setAutoDefScope(source[4])
+  }
+  definitionSplitter(definition = null){
+    if (!definition)
+      return this
+    const found = [...definition.matchAll(/^(\[(\S+)\])?(sta|din)?(gen|com|ins)?(\w+)$/gi)]
+    if (!found || found[0] == null || found[0][5] == null || found[0][5] === '')
+      return this
+    return this.loadAutoDefTypes(found[0]).loadAutoDefMode(found[0]).loadAutoDefScope(found[0]).setAutoDefName(found[0][5])
+  }
+  preCamel (prefix = '') {
+    return camelCase(`${prefix} ${this.getAutoDefName()}`)
+  }
+  calAutoPropName(prefix = '') {
+    return camelCase(`${prefix} ${this.getAutoDefMode()} ${this.getAutoDefScope()} ${this.getAutoDefName()}`)
+  }
+/**
+ * add data
+ */
+  addAutoData(){
+    if (this.hasAutoData())
+      this.autoData[this.calAutoPropName()] = this.getAutoDefDataInit()
+    return this
+  }
+/**
+ * add prop
+ */
+  addAutoProp(){
+    if (this.hasAutoProp())
+      this.autoProps.push(this.calAutoPropName('cv'))
+    return this
+  }
+/**
+ * add computed
+ */
+  addAutoComputed(){
+    let name = this.calAutoPropName()
+    if (this.hasAutoData()) {
+      this.autoComputed[this.calAutoPropName('cd')] = function() {
+        return this[name]
+      }
+    }
+    if (this.hasAutoProp()) {
+      let defaultComputedProp = this.getAutoDefPropInit()
+      let propName = this.calAutoPropName('cv')
+      this.autoComputed[this.calAutoPropName('cp')] = function() {
+        let comp = this[propName]
+        return comp != null ? comp : defaultComputedProp
+      }
+    }
+    return this
+  }
+/**
+ * add method
+ */
+  addAutoMethod(){
+    if (this.hasAutoData()) {
+      let name = this.calAutoPropName()
+      this.autoMethods[this.calAutoPropName('m set')] = function(value = null) {
+        this.$set(this,name,value)
+        return this
+      }
+    }
+    return this
+  }
+/**
+ * add emitter
+ */
+  addAutoEmitter(){
+    if (this.hasAutoEmitter()) {
+      let name = this.calAutoPropName()
+      this.autoMethods[camelCase(`em ${name} proccesor`)] = function(emitted = null) {
+        return new Promise ((resolve, reject) => resolve(emitted))
+      }
+      let propName = this.calAutoPropName()
+      if(this.autoMethods[camelCase(`em ${propName} emitter`)] == null)
+        this.autoMethods[camelCase(`em ${propName} emitter`)] = function(emitted = null) {
+          this[camelCase(`em ${propName} proccesor`)](emitted).then((proccesed = null) => {
+            this.$emit(kebabCase(`em ${propName} event`), proccesed)
+          }).catch(error=>error)
+        }
+    }
+    return this
+  }
+/**
+ * add mounter
+ */
+  addAutoMounter(){
+    if (this.hasAutoProp() && this.getAutoDefMode() === 'sta') {
+      let propName = this.calAutoPropName('cp')
+      let dataName = this.calAutoPropName('m set')
+      this.autoMounters.push(function (context) {
+        context[dataName](context[propName])
+      })
+    }
+    return this
+  }
+/**
+ * add mounted
+ */
+  addAutoMounted(){
+    this.autoMounted = (function(autoMounters){
+      return function () {
+        for (const mounter of autoMounters)
+          mounter(this)
+      }
+    })(this.autoMounters)
+    return this
+  }
+  complementDefinition () {
+    if(this.getAutoDefMode() === 'sta'){
+      this.setAutoDefPropInit(this.getAutoDefInit())
+      this.getAutoDefTypes()['D']=true
+      this.getAutoDefTypes()['P']=true
+    }else{
+      this.setAutoDefPropInit(!this.hasAutoData() ? this.getAutoDefInit() : null)
+    }
+    return this
+  }
+  validateDefinition () {
+    if (!this.getAutoDefName())
+      return false
+    if(this.hasAutoProp() && (!this.getAutoDefMode() || !this.getAutoDefScope()))
+      return false
+    return true
+  }
+  /**
+  * Mirroring property list
+  * property has next structure {'property':{'init':initializationValue,'mode':'D|P|C|E|CD'}}
+  * D(ata)
+  * P(rops)
+  * EM(miter)
+  * undefined everything
+  * @return vue structure
+  */
+  fixProperties (properties = null) {
+    for (const [property, def] of Object.entries(properties)) {
+      if (this.definitionSplitter(property).setAutoDefInit(def).complementDefinition().validateDefinition())
+        this.addAutoData().addAutoProp().addAutoComputed().addAutoMethod().addAutoEmitter().addAutoMounter()
+    }
+    return this.addAutoMounted().getAutos()
+  }
+
+  validPropertySegments (splitedProperty = []) {
+    return this.validModes[splitedProperty[1]] != null && this.validScopes[splitedProperty[2]] != null
+  }
+
+  complementName (splited,from = 3){
+    let complementedName = ''
+    for (let i=from;i<splited.length; i++)
+      complementedName += ` ${splited[i]}`
+    return complementedName
+  }
+
+  fixPropertyName (splitedProperty = []) {
+    switch(splitedProperty[2]){
+      case 'com':
+        return camelCase(`${splitedProperty[1]} ${splitedProperty[2]}f ${this.componentConnector.getCurrentComponent().tag} ${this.complementName(splitedProperty)}`)
+      case 'comf':
+        return camelCase(`${splitedProperty[1]} ${splitedProperty[2]} ${this.complementName(splitedProperty)}`)
+      case 'ins':
+        return camelCase(`${splitedProperty[1]} ${splitedProperty[2]}f ${this.componentConnector.getParentPrefix()} ${this.componentConnector.getCurrentComponent().tag} ${this.complementName(splitedProperty)}`)
+      case 'insf':
+        return camelCase(`${splitedProperty[1]} ${splitedProperty[2]} ${this.componentConnector.getParentPrefix()} ${this.complementName(splitedProperty)}`)
+    }
+    return camelCase(`${splitedProperty[1]} ${splitedProperty[2]} ${this.complementName(splitedProperty)}`)
+  }
+
+  validEmitterSegments (splitedEmitter = []) {
+    return this.validModes[splitedEmitter[1]] != null && this.validScopes[splitedEmitter[2]] != null
+  }
+
+  fixEmitterName (splitedEmitter = []) {
+    switch(splitedEmitter[2]){
+      case 'com':
+        return camelCase(`${splitedEmitter[1]} ${splitedEmitter[2]}f ${this.componentConnector.getCurrentComponent().tag} ${this.complementName(splitedEmitter)}`)
+      case 'comf':
+        return camelCase(`${splitedEmitter[1]} ${splitedEmitter[2]} ${this.complementName(splitedEmitter)}`)
+      case 'ins':
+        return camelCase(`${splitedEmitter[1]} ${splitedEmitter[2]}f ${this.componentConnector.getParentPrefix()} ${this.componentConnector.getCurrentComponent().tag} ${this.complementName(splitedEmitter)}`)
+      case 'insf':
+        return camelCase(`${splitedEmitter[1]} ${splitedEmitter[2]} ${this.componentConnector.getParentPrefix()} ${this.complementName(splitedEmitter)}`)
+    }
+    return camelCase(`${splitedEmitter[1]} ${splitedEmitter[2]} ${this.complementName(splitedEmitter)}`)
+  }
+}
---
+++ src/VueComponentConnector.js
@@ -0,0 +1,307 @@
+import {camelCase,kebabCase,replace,startsWith,endsWith,split} from 'lodash'
+
+export default class VueMirroring {
+  constructor (parentPrefix = null) {
+    this.parentPrefix     = parentPrefix
+    this.components       = {}
+    this.currentComponent = {}
+    this.data             = {}
+    this.props            = []
+    this.computed         = {}
+    this.bindings         = {}
+    this.methods          = {}
+    this.ons              = {}
+    this.mode             = 'normal'
+  }
+
+  inyectAutoDefiner (autoDefiner = null) {
+    this.autoDefiner = autoDefiner
+    return this
+  }
+
+  bindsMirroring () {
+    for (const [key, currentComponent] of Object.entries(this.getComponents()))
+      this.setCurrentComponent(currentComponent).bindMirroring().getComponents()[key] = this.getCurrentComponent()
+    this.methods['mBinding'] = (function(bindings){
+      return function (index = null) {
+        if (!index || bindings == null || bindings[index] == null)
+          return {}
+        return bindings[index](this)
+      }
+    })(this.bindings)
+
+    this.methods['mOns'] = (function(ons){
+      return function (index = null) {
+        let events = {}
+
+        if (index && ons != null && ons[index] != null)
+          for (const [event, emitter] of Object.entries(ons[index]))
+            events[event] = emitter(this)
+
+        return events
+      }
+    })(this.ons)
+    return this
+  }
+
+  binding (){
+    let nData = this.data
+    return {
+      data () {
+        return nData
+      },
+      props    : this.props,
+      computed : this.computed,
+      methods  : this.methods,
+    }
+  }
+
+  validProperty (splitedProperty = []) {
+    if (
+      splitedProperty[0] == null ||
+      splitedProperty[1] == null ||
+      splitedProperty[2] == null ||
+      splitedProperty[3] == null ||
+      splitedProperty[0] !== 'cv'
+    )
+      return false
+    return this.autoDefiner.validPropertySegments(splitedProperty)
+  }
+
+  validEmitter (splitedEmitter = []) {
+    if (
+      splitedEmitter[0] == null ||
+      splitedEmitter[1] == null ||
+      splitedEmitter[2] == null ||
+      splitedEmitter[3] == null ||
+      splitedEmitter[0] !== 'em' ||
+      splitedEmitter[splitedEmitter.length-1] !== 'emitter'
+    )
+      return false
+    return this.autoDefiner.validEmitterSegments(splitedEmitter)
+  }
+
+  fixPropertyName (splitedProperty = []) {
+    return this.autoDefiner.fixPropertyName(splitedProperty)
+  }
+
+  fixEmitterName (splitedEmitter = []) {
+    return this.autoDefiner.fixEmitterName(splitedEmitter)
+  }
+
+  complementName (splited,from = 3) {
+    return this.autoDefiner.complementName(splited,1)
+  }
+
+  bindMirroring () {
+    let mirroring = {
+      props    : [],
+      computed : {},
+      methods  : {}
+    }
+    var binding = {}
+    let tag = kebabCase(`${this.getCurrentComponent().tag}`)
+    if(this.getCurrentComponent().props){
+      for (const [position, prop] of Object.entries(this.getCurrentComponent().props)) {
+        let splitedProperty = split(kebabCase(prop),'-')
+        if(!this.validProperty(splitedProperty)){
+          this.getCurrentComponent().props.splice(position, 1)
+          continue
+        }
+        let property = this.fixPropertyName(splitedProperty)
+
+        let cvProperty       = camelCase('cv ' + property)
+        let clProperty       = camelCase('cl ' + property)
+        let cpProperty       = camelCase('cp ' + property)
+        let cdProperty       = camelCase('cd ' + property)
+        let match = false
+        for (const prop of this.props)
+          if(cvProperty === prop){
+            match = true
+            continue
+          }
+        if(!match)
+          this.props.push(cvProperty)
+        mirroring.props.push(cvProperty)
+        this.computed[cpProperty] = function() {
+          if (this[cvProperty] != null)
+            return this[cvProperty]
+          if (this[clProperty] != null)
+            return this[clProperty]
+          return null
+        }
+        binding[kebabCase(`cv-${property}`)] = cpProperty
+      }
+    }
+    let methodNames = Object.keys(this.getCurrentComponent().methods)
+    if(methodNames.length){
+      for (const [currentMethod, currentCallBack] of Object.entries(this.getCurrentComponent().methods)){
+        let splitedEmitter = split(kebabCase(currentMethod),'-')
+        if(!this.validEmitter(splitedEmitter))
+          continue
+        splitedEmitter.pop()
+
+        let emitter         = this.fixEmitterName(splitedEmitter)
+        let originalEmitter = camelCase(this.complementName(splitedEmitter,1))
+
+        let newProccesorName = camelCase(`em ${emitter} Proccesor`)
+        let newEmitterName   = camelCase(`em ${emitter} Emitter`)
+        let newEventName     = kebabCase(`em ${emitter} event`)
+        let test = this.getParentPrefix()
+        this.methods[newProccesorName] = function(emitted = null) {
+          console.log([test,newProccesorName])
+          return new Promise ((resolve, reject) => resolve(emitted))
+        }
+        this.methods[newEmitterName] = (function(newProccesorName,newEventName){
+          return function(emitted = null) {
+            this[newProccesorName](emitted).then((proccesed = null) => {
+              this.$emit(newEventName, proccesed)
+            }).catch(error=>error)
+          }
+        })(newProccesorName,newEventName)
+        if (this.ons[tag] == null)
+          this.ons[tag] = {}
+        this.ons[tag][kebabCase(`em ${originalEmitter} event`)]=(function(newEmitterName){
+          return function(context){
+            return context[newEmitterName]
+          }
+        })(newEmitterName)
+      }
+    }
+    this.bindings[tag] = (function(binding,tag,posFix){
+      return function (context) {
+        let fixedBinding = {}
+        for (const [childProperty, parentComputed] of Object.entries(binding))
+          fixedBinding[childProperty] = context[parentComputed]
+
+        fixedBinding['ref'] = camelCase(`${tag} ${posFix} ref`)
+        return fixedBinding
+      }
+    })(binding,tag,this.getCurrentComponent().posFix)
+
+    this.getCurrentComponent().props = mirroring.props
+    return this
+  }
+
+  mixinsMirroring () {
+    for (const [key, currentComponent] of Object.entries(this.getComponents())){
+      this.setCurrentComponent(currentComponent)
+        .mixinMirroring(currentComponent.component)
+        .getComponents()[key] = this.getCurrentComponent()
+    }
+    return this
+  }
+
+  mixinMirroring (component = null) {
+    if (!component)
+      return this
+    if (component.props != null){
+      for (const currentProp of component.props){
+        let match = false
+        if (this.getCurrentComponent().props == null)
+          this.getCurrentComponent().props = []
+        for (const prop of this.getCurrentComponent().props){
+          if(currentProp === prop){
+            match = true
+            break
+          }
+        }
+        if(!match){
+          this.getCurrentComponent().props.push(currentProp)
+        }
+      }
+    }
+    if (component.methods != null){
+      for (const [currentMethod, currentCallBack] of Object.entries(component.methods)){
+        if(!startsWith(currentMethod,'em') || !endsWith(currentMethod,'Emitter'))
+          continue
+
+        let match = false
+        if (this.getCurrentComponent().methods == null)
+          this.getCurrentComponent().methods = {}
+          for (const [method, callBack] of Object.entries(this.getCurrentComponent().methods)){
+            if(currentMethod === method){
+              match = true
+              break
+            }
+        }
+        if(!match)
+          this.getCurrentComponent().methods[currentMethod] = currentCallBack
+      }
+    }
+    if (component.mixins != null)
+      for (let currentComponent of component.mixins)
+        this.mixinMirroring(currentComponent)
+    if (component.extends != null)
+      this.mixinMirroring(component.extends)
+    return this
+  }
+
+  getParentPrefix () {
+    return this.parentPrefix || ''
+  }
+  getProps () {
+    return this.props || []
+  }
+  getComponents () {
+    return this.components || {}
+  }
+  getCurrentComponent () {
+    return this.currentComponent || {}
+  }
+  getMode () {
+    return this.mode || 'normal'
+  }
+
+  setParentPrefix (parentPrefix = null) {
+    this.parentPrefix = parentPrefix || ''
+    return this
+  }
+
+  setProps (props = null) {
+    this.props = props || []
+    return this
+  }
+  setComponents (components) {
+    this.components = components || {}
+    return this
+  }
+  setCurrentComponent (currentComponent) {
+    this.currentComponent = currentComponent || {}
+    return this
+  }
+  setMode (mode) {
+    this.mode = mode || 'normal'
+    return this
+  }
+  enableRootMode(){
+    return this.setMode('root')
+  }
+  isRoot(){
+    return this.getMode() === 'root'
+  }
+  fixedComponentTag (component = null){
+    if (!component || component.__file == null)
+      return ''
+
+    const found = [...component.__file.matchAll(/^.*?(Cv|cv|cV)(\w+)\.(js|vue)$/g)]
+    if (found[0] == null || found[0][2] == null)
+      return ''
+    return kebabCase(found[0][2])
+  }
+  loadComponents (...components) {
+    let fixedComponents = {}
+    for (let currentComponent of components){
+      let tag = Object.keys(currentComponent)[0]
+      let posFix = currentComponent.posFix != null ? currentComponent.posFix : ''
+      fixedComponents[kebabCase(`${tag} ${posFix}`)]={
+        tag       : tag,
+        component : currentComponent[tag],
+        posFix    : currentComponent.posFix != null ? currentComponent.posFix : '',
+        props     : currentComponent.props != null ? currentComponent.props   : [],
+        methods   : currentComponent.methods != null ? currentComponent.methods   : {}
+      }
+    }
+    return this.setComponents(fixedComponents)
+  }
+}
