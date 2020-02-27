df036db374d6588e7f96251158c6aaf0d90251aa
Benomas <benomas@gmail.com>
2020-02-26 19:13:24 -0600

    add last changes with fast commit

--- src/directives/cvError.js
+++ src/directives/cvError.js
@@ -1,15 +1,54 @@
+let defaultClasses = [
+  'w-100',
+  'f-s12px',
+  'txt-negative',
+  'f-left',
+  'q-pl-lg'
+]
+
+let errorWrapper = function (el, binding, vnode) {
+  if (binding.value == null)
+    return
+
+  let field   = binding.value.field
+  let context = vnode.context
+
+  if (context.cErrors == null) {
+    el.textContent = ''
+    return
+  }
+
+  if (context.cErrors[field] == null) {
+    el.textContent = ''
+    return
+  }
+
+  let newDefaultClasses = binding.value.classes != null ? binding.value.classes : defaultClasses
+  let errorClases = {}
+  for (const defaultClass of newDefaultClasses)
+    errorClases[defaultClass] = false
+
+  for (const classDef of el.classList)
+    if (errorClases[classDef] != null)
+      errorClases[classDef] = true
+
+  for (const [ClassDef, alreadyExist] of Object.entries(errorClases))
+    if (!alreadyExist)
+      el.classList.add(ClassDef)
+
+  el.textContent = context.cErrors[field][0]
+}
+
 export default {
-  /*
-  inserted () {},
-  update () {},
-  componentUpdated () {},
-  */
-  bind (el, binding, vnode) {
-    let slotName   = binding.value
-    let context = vnode.context
-    context.$set(context,slotName,true)
+  update (el, binding, vnode) {
+    errorWrapper(el, binding, vnode)
   },
 
-  unbind (el, binding, vnode) {
-  }
+  componentUpdated (el, binding, vnode) {
+    errorWrapper(el, binding, vnode)
+  },
+
+  bind (el, binding, vnode) {
+    errorWrapper(el, binding, vnode)
+  },
 }
